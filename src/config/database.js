86fd0bbc09ca5019ab7537e328a422c0d2dbf4e2
TTamosauskas <thiago.gt@gmail.com>
2020-02-05 19:20:41 -0300

    add: new files and dependencies

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgress',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscore: true,
+    underscoredAll: true,
+  },
+
+};
