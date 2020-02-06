0f60bf53e903084984b54382b8239a186dfde0b9
TTamosauskas <thiago.gt@gmail.com>
2020-02-06 16:00:05 -0300

    feat: user regstration

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,8 @@
 import { Router } from "express";
-import User from './app/models/User';
+import UserController from "./app/controllers/UserController";
 
 const routes = new Router();
 
-routes.get("/", async (req, res) => {
-
-  const user = await User.create({
-    name: "Diego Fernandes",
-    email: "diego@gmail.com",
-    password_hash: "12121212",
-  });
-
-  return res.json({user});
-});
+routes.post('/users', UserController.store);
 
 export default routes;
